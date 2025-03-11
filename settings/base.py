e6c8122d7036ffb88309b79f9e2d3a284c744460
Tresor <tresorkabis@gmail.com>
2025-03-11 13:29:58 +0100

    maj

--- settings/base.py
+++ settings/base.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'import_export',
+
     'intern',
     'training',
     'users',
--- training/models.py
+++ training/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Filiere(models.Model):
+    nom = models.CharField(max_length=200)
+
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nom
+class Formation(models.Model):
+    nom = models.CharField(max_length=200)
+    duree = models.IntegerField(default=0)
+    filiere = models.ForeignKey(Filiere, on_delete=models.SET_NULL, null=True, blank=True)
+
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nom
