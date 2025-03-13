e261bbfe3a994a84933a1f34a113353301e2dc03
nachi-lema <nachilema175@gmail.com>
2025-03-13 16:10:10 +0100

    maj

--- intern/urls.py
+++ intern/urls.py
@@ -10,4 +10,6 @@ from intern.views.stagiaire_views import StagiaireDetailView, StagiaireListView
 urlpatterns = [
     path("categories", CategorieListView.as_view(), name="categories"),
     path("stagiaires", StagiaireListView.as_view(), name="stagiaires"),
+    path("categories/<int:pk>", CategorieDetailView.as_view(), name="categorie"),
+    path("stagiaires/<int:pk>", StagiaireDetailView.as_view(), name="stagiaire"),
 ]
