ddd2c058bd26af105e9c308bd15ff15dd2d73e51
Tresor <tresorkabis@gmail.com>
2025-03-13 15:51:58 +0100

    maj urls global

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path("i18n/", include("django.conf.urls.i18n")),
 
+    path("intern/", include('intern.urls')),
+    path("progress/", include('progress.urls')),
     path("", include('training.urls')),
+    path("users/", include('users.urls')),
 ]
 
 #urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
