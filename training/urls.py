ae1f54ac02133ddc3981e1aa3b98eb24d0f1575b
Tresor <tresorkabis@gmail.com>
2025-03-13 13:43:37 +0100

    detail view formation

--- training/urls.py
+++ training/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from training.views.formation_views import FormationListView
+from training.views.formation_views import FormationDetailView, FormationListView
 from training.views.home_views import HomeView
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("formations", FormationListView.as_view(), name="formations"),
+    path("formations/<int:pk>", FormationDetailView.as_view(), name="formation"),
 ]
