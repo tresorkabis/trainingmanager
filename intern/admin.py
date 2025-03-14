cc02a2bc6e648015262712af62520c18500cbce9
senda-cedrick <sendamutombo@gmail.com>
2025-03-14 17:58:08 +0100

    ajou

--- intern/admin.py
+++ intern/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from training.models import Filiere, Formation, Service
+
+admin.site.register(Service)
+admin.site.register(Filiere)
+admin.site.register(Formation)
