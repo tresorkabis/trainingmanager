000757a3c22590b46dc2cf9f9a3567b74ba33e76
Tresor <tresorkabis@gmail.com>
2025-03-19 23:10:12 +0100

    maj

--- training/views/formation_views.py
+++ training/views/formation_views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from training.models import Formation
+from training.models import Filiere, Formation
 
 class FormationListView(ListView):
     context_object_name = "formation_list"
     queryset = Formation.objects.all()
+    paginate_by = 3
     template_name = "training/formations.html"
 
 class FormationDetailView(DetailView):
@@ -17,7 +18,24 @@ class FormationDetailView(DetailView):
 
 class FormationCreateView(View):
     def get(self, request):
-        return render(request, 'training/formation.html')
+        filieres = Filiere.objects.all()
+        ctx = {
+            "filieres":filieres
+        }
+        return render(request, 'training/formation.html', ctx)
     
     def post(self, request):
-        return HttpResponseRedirect("/formations")
+        nom = request.POST['nom']
+        duree = request.POST['duree']
+        id_filiere = request.POST['filiere']
+        fraismateriels = request.POST['fraism']
+
+        formation = Formation(
+            nom = nom,
+            duree = duree,
+            filiere_id = id_filiere,
+            fraismateriels = fraismateriels
+        )
+        formation.save()
+
+        return HttpResponseRedirect("/training/formations")
