ee52056df98d1bb51bb25856cbbfa22682d56366
Tresor <tresorkabis@gmail.com>
2025-03-22 21:42:37 +0100

    dashboard

--- training/views/home_views.py
+++ training/views/home_views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from intern.models import Stagiaire
+from progress.models import Action
+from training.models import Filiere, Formation
+
 class HomeView(View):
     def get(self, request):
         ctx = {
-            "link":"home"
+            "link":"home",
+            "nbformations" : Formation.objects.count(),
+            "nbfilieres" : Filiere.objects.count(),
+            "nbstagiaires" : Stagiaire.objects.count(),
+            "nbactions" : Action.objects.count()
         }
         return render(request, "home/index.html", ctx)
