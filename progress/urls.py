850ad5da4cce99d692a1335730b723649a0ff48d
senda-cedrick <sendamutombo@gmail.com>
2025-03-23 22:28:22 +0100

    cv

--- progress/urls.py
+++ progress/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from progress.views.action_views import ActionListViews
 from progress.views.detailAction_views import DetailActionListViews
 from progress.views.formateur_views import FormateurListView
