8cc2ea7c909c9818fe011b0d1ad33f4428ed75ea
tresor <tresorkabis@gmail.com>
2026-06-20 23:10:09 +0100

    maj

--- migrations.sh
+++ migrations.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Supprimer les anciennes migrations (sauf __init__.py)
+echo "Removing old migration files..."
+find . -type f -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -type d -path "*/migrations/__pycache__" -exec rm -r {} +
+
 echo "Running makemigrations for all apps..."
 DJANGO_SETTINGS_MODULE=settings.dev python manage.py makemigrations users intern training progress
 
